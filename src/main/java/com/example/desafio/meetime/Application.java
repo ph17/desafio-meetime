77292fa7ae61717c616171d774225d55042b8145
Pedro Henrique Lemos <pedrohenriquelemos17@gmail.com>
2025-04-04 11:35:53 -0300

    inicio do projeto

---
+++ src/main/java/com/example/desafio/meetime/Application.java
@@ -0,0 +1,13 @@
+package com.example.desafio.meetime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
