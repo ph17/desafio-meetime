c0ea2ef4e5deaae702a3f7c1f0b781a7cae107bd
Pedro Henrique Lemos <pedrohenriquelemos17@gmail.com>
2025-04-04 22:11:34 -0300

    propriedades para autorização

--- src/main/java/com/example/desafio/meetime/config/properties/HubSpotAuthProperties.java
+++ src/main/java/com/example/desafio/meetime/config/properties/HubSpotAuthProperties.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 public record HubSpotAuthProperties(String url,
                                     String clientId,
+                                    String clientSecret,
                                     String redirectUri,
+                                    String grantType,
                                     List<String> requiredScopes,
                                     List<String> optionalScopes) {
 }
