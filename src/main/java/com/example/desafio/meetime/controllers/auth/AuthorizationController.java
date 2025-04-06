6077a5d03fbea9762891717feb7b048364a0e338
Pedro Henrique Lemos <pedrohenriquelemos17@gmail.com>
2025-04-06 11:25:37 -0300

    adicionando prefixo auth

--- src/main/java/com/example/desafio/meetime/controllers/auth/AuthorizationController.java
+++ src/main/java/com/example/desafio/meetime/controllers/auth/AuthorizationController.java
@@ -1,15 +1,15 @@
-package com.example.desafio.meetime.controllers;
+package com.example.desafio.meetime.controllers.auth;
 
 import com.example.desafio.meetime.dto.AuthorizationResponseDTO;
-import com.example.desafio.meetime.integration.response.HubAuthFeignResponse;
-import com.example.desafio.meetime.mappers.AuthorizationMapper;
 import com.example.desafio.meetime.services.AuthorizationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthorizationController {
     private final AuthorizationService service;
