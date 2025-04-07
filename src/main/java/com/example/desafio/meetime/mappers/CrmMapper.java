f57487eec65b94570eb25196d3e71edcea8fce68
Pedro Henrique Lemos <pedrohenriquelemos17@gmail.com>
2025-04-07 07:35:22 -0300

    import

--- src/main/java/com/example/desafio/meetime/mappers/CrmMapper.java
+++ src/main/java/com/example/desafio/meetime/mappers/CrmMapper.java
@@ -3,7 +3,6 @@ package com.example.desafio.meetime.mappers;
 import com.example.desafio.meetime.dto.crm.CrmCreateContactRequestDTO;
 import com.example.desafio.meetime.dto.crm.CrmCreateContactResponseDTO;
 import com.example.desafio.meetime.integration.crm.request.HubCrmContactProperties;
-import com.example.desafio.meetime.integration.crm.request.HubCrmCreateContactRequest;
 import com.example.desafio.meetime.integration.crm.response.HubCrmCreateContactResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
