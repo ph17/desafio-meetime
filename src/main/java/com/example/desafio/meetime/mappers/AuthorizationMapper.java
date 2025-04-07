6d9ef44d81d2c788df640076726110af1625050d
Pedro Henrique Lemos <pedrohenriquelemos17@gmail.com>
2025-04-07 17:28:40 -0300

    adicionando "Bearer" no token de resposta

--- src/main/java/com/example/desafio/meetime/mappers/AuthorizationMapper.java
+++ src/main/java/com/example/desafio/meetime/mappers/AuthorizationMapper.java
@@ -2,7 +2,10 @@ package com.example.desafio.meetime.mappers;
 
 import com.example.desafio.meetime.dto.auth.AuthorizationResponseDTO;
 import com.example.desafio.meetime.integration.auth.response.HubAuthFeignResponse;
+import jdk.jfr.Name;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -10,5 +13,11 @@ public interface AuthorizationMapper {
 
     AuthorizationMapper INSTANCE = Mappers.getMapper(AuthorizationMapper.class);
 
+    @Mapping(target = "accessToken", qualifiedByName = "addBearerAccessToken")
     AuthorizationResponseDTO toAuthorizationDTO(HubAuthFeignResponse response);
+
+    @Named("addBearerAccessToken")
+    default String addBearerAccessToken(String token){
+        return "Bearer " + token;
+    }
 }
